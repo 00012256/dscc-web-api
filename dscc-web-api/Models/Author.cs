429e35eaea98214e9ac2586cc6e65438b0610d36
00012256 <00012256@gmail.com>
2023-11-05 18:05:20 +0500

    init: create models

---
+++ dscc-web-api/Models/Author.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace dscc_web_api.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Biography { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ dscc-web-api/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dscc_web_api.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime LastUpdated { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+    }
+}

e9b886103d7be2cde29e06d2902c6e075f158949
00012256 <00012256@gmail.com>
2023-11-05 19:43:26 +0500

    feat: add controllers

---
+++ dscc-web-api/Controllers/AuthorsController.cs
@@ -0,0 +1,75 @@
+﻿using dscc_web_api.Models;
+using dscc_web_api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dscc_web_api.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorsController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = _authorRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{id}", Name = "GetAuthor")]
+        public IActionResult GetAuthor(int id)
+        {
+            var author = _authorRepository.GetAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor([FromBody] Author author)
+        {
+            if (author == null)
+            {
+                return BadRequest();
+            }
+            _authorRepository.InsertAuthor(author);
+            return CreatedAtRoute("GetAuthor", new { id = author.AuthorId }, author);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor(int id, [FromBody] Author author)
+        {
+            if (author == null || id != author.AuthorId)
+            {
+                return BadRequest();
+            }
+            var existingAuthor = _authorRepository.GetAuthorById(id);
+            if (existingAuthor == null)
+            {
+                return NotFound();
+            }
+            _authorRepository.UpdateAuthor(author);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var author = _authorRepository.GetAuthorById(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            _authorRepository.DeleteAuthor(id);
+            return NoContent();
+        }
+    }
+}
---
+++ dscc-web-api/Controllers/PostsController.cs
@@ -0,0 +1,75 @@
+﻿using dscc_web_api.Models;
+using dscc_web_api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dscc_web_api.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostsController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var posts = _postRepository.GetPosts();
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}", Name = "GetPost")]
+        public IActionResult GetPost(int id)
+        {
+            var post = _postRepository.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost([FromBody] Post post)
+        {
+            if (post == null)
+            {
+                return BadRequest();
+            }
+            _postRepository.InsertPost(post);
+            return CreatedAtRoute("GetPost", new { id = post.PostId }, post);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePost(int id, [FromBody] Post post)
+        {
+            if (post == null || id != post.PostId)
+            {
+                return BadRequest();
+            }
+            var existingPost = _postRepository.GetPostById(id);
+            if (existingPost == null)
+            {
+                return NotFound();
+            }
+            _postRepository.UpdatePost(post);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePost(int id)
+        {
+            var post = _postRepository.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            _postRepository.DeletePost(id);
+            return NoContent();
+        }
+    }
+}
